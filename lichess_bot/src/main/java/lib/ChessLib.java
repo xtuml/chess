55724e02ca84f4dad304b89390e98ce47da3eb92
Levi Starrett <levi@roxsoftware.com>
2024-04-23 13:17:11 -0400

    Update ChessLib EE; handle draw offers

--- lichess_bot/src/main/java/lib/ChessLib.java
+++ lichess_bot/src/main/java/lib/ChessLib.java
@@ -8,15 +8,54 @@ import com.github.bhlangonijr.chesslib.move.Move;
 import com.github.bhlangonijr.chesslib.move.MoveList;
 
 public class ChessLib {
-	
-	public static List<String> legalMoves(List<String> moves) {
+
+	public static final String STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+	public static List<String> legalMoves(String fen) {
 		final var board = new Board();
+		board.loadFromFen(fen);
+		return board.legalMoves().stream().map(Move::toString).collect(Collectors.toList());
+	}
+
+	public static String sourceFile(String move) {
+		if (move.matches("[a-h][1-8][a-h][1-8]")) {
+			return "" + move.charAt(0);
+		} else {
+			throw new IllegalArgumentException("Not a valid chess move: " + move);
+		}
+	}
+
+	public static int sourceRank(String move) {
+		if (move.matches("[a-h][1-8][a-h][1-8]")) {
+			return move.charAt(1) - '0';
+		} else {
+			throw new IllegalArgumentException("Not a valid chess move: " + move);
+		}
+	}
+
+	public static String destFile(String move) {
+		if (move.matches("[a-h][1-8][a-h][1-8]")) {
+			return "" + move.charAt(2);
+		} else {
+			throw new IllegalArgumentException("Not a valid chess move: " + move);
+		}
+	}
+
+	public static int destRank(String move) {
+		if (move.matches("[a-h][1-8][a-h][1-8]")) {
+			return move.charAt(3) - '0';
+		} else {
+			throw new IllegalArgumentException("Not a valid chess move: " + move);
+		}
+	}
+
+	public static String movesToFEN(List<String> moves) {
 		if (moves.size() > 0) {
 			final var moveList = new MoveList();
 			moveList.loadFromSan(moves.stream().collect(Collectors.joining(" ")));
-			board.loadFromFen(moveList.getFen());
+			return moveList.getFen();
 		}
-		return board.legalMoves().stream().map(Move::toString).collect(Collectors.toList());
+		return STARTPOS;
 	}
 
 }
